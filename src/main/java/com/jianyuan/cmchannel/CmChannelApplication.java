a608047c03d1a91b0279e07cb12ff1c833fdab78
guocehua <m15510260561@163.com>
2019-10-18 16:35:47 +0800

    完成上传接口

---
+++ src/main/java/com/jianyuan/cmchannel/CmChannelApplication.java
@@ -0,0 +1,13 @@
+package com.jianyuan.cmchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmChannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmChannelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jianyuan/cmchannel/CmChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianyuan.cmchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
